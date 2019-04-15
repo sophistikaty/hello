7e40a8986a08a6a861412cc32542763c0633d747
sophistikaty <sophistikaty@users.noreply.github.com>
2019-04-14 20:30:23 -0400

    Better repeated string solution.

--- repeatedString.js
+++ repeatedString.js
@@ -3,24 +3,30 @@
  * https://www.hackerrank.com/challenges/repeated-string/problem
  */
 
- function buildString(ogString, length){
-     let testString = ogString;
-     while(testString.length < length){
-        testString = testString + ogString;
-     }
-     return testString.substr(0, length); 
- }
+function isA(letterCount, character){
+    const isA = character === 'a';
+    return isA ? letterCount + 1 : letterCount;
+};
 
-function repeatedString(pattern, length) {
-    return buildString(pattern, length).split('').reduce(function(letterCount, character){
-        console.log('letterCount, character ', letterCount, character);
-        const isA = character === 'a';
-        return isA ? letterCount + 1 : letterCount;
-    },0);
+function countFromWholePattern(stringPattern, length){
+    const countInOriginal = stringPattern.split('').reduce(isA,0);
+    const wholePatternLength = parseInt(length / stringPattern.length, 10);
+    return wholePatternLength * countInOriginal;
 }
 
-const stringPattern = 'aba';
-const lengthInt = 10;
+function countFromRemainder(stringPattern, length){
+    const remainderLength = length % stringPattern.length;
+    const partialPattern = stringPattern.substr(0, remainderLength);
+    return partialPattern.split('').reduce(isA,0);
+}
+
+function repeatedString(pattern, length) {
+    return countFromWholePattern(pattern, length)
+        + countFromRemainder(pattern, length);
+}
+
+const stringPattern = 'a';
+const lengthInt = 1000000000000;
 
 console.log('There are ', repeatedString(stringPattern, lengthInt),
  ' occurences of the letter a in the first ', lengthInt, ' of the repeated string pattern');
