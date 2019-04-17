8ce70a3f321d5b6049c3b663bf22a5a09a21ffd0
Kaitlynn Hendricks <khendricks@researchinnovations.com>
2019-04-17 15:49:12 -0400

    hashmap tho (and better return bool logic.)

--- ransomNote.js
+++ ransomNote.js
@@ -3,19 +3,27 @@
  * https://www.hackerrank.com/challenges/ctci-ransom-note/problem
  */
 
+ function magazineToHashMap(magazine){
+     return magazine.reduce(function(map, word){
+        const wordCount = map[word];
+        map[word] = wordCount ? wordCount +1 : 1; 
+        return map;
+     }, {})
+ }
+
  function checkMagazine(magazine, note){
+    const magazineWords = magazineToHashMap(magazine);
     const canReproduce = note.every(function(word){
-        let wordAvailable = false;
-        const wordIndex = magazine.indexOf(word);
-        if(wordIndex !== -1){
-            magazine.splice(wordIndex, 1);
-            wordAvailable = true;
+        const wordCountAvailable = magazineWords[word];
+        if(wordCountAvailable > 0){
+            magazineWords[word] = wordCountAvailable -1;
         }
-        return wordAvailable;
+        return wordCountAvailable ? true : false;
     })
-    return canReproduce ? 'Yes' : 'No';
+    console.log(canReproduce ? 'Yes' : 'No');
  }
  const magazine = ['give', 'me', 'one', 'grand', 'today', 'night'];
  const note = ['give', 'one', 'grand', 'today'];
  console.log('will harold be able to reproduce his (case sensitive)'
- + 'ransom note from the full words in the magazine? ', checkMagazine(magazine, note));
+ + 'ransom note from the full words in the magazine? ');
+ checkMagazine(magazine, note);
